aef236047f3b950757bffe16db0b9e9d9f428e12
amShantanu <golush22@gmail.com>
2023-01-17 21:02:55 +0530

    in table

--- printarray.js
+++ printarray.js
@@ -1,3 +1,25 @@
-const Arr = [1, 2, 3];
-console.log(Arr[1]);
-// hi 123
+let arr = [2, -2, 6, 8, 3, -4, 9, -8, -6, -5];
+let x = arr.length;
+arr.sort((a, b) => b - a);
+let pos = [];
+let neg = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    pos.push(arr[i]);
+  } else {
+    neg.push(arr[i]);
+  }
+}
+console.log(pos);
+console.log(neg);
+
+let altArr = pos
+  .map(function (s, i) {
+    return [s, neg[i]];
+  })
+  .reduce(function (a, b) {
+    return a.concat(b);
+  });
+
+console.table(altArr);
